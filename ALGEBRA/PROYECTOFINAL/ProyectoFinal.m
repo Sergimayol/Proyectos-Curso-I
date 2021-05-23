4aef18e16d9ece92ae5a454c54e6d4a00236eb82
alerodriargui <80339818+alerodriargui@users.noreply.github.com>
2021-05-23 23:29:22 +0200

    Update ProyectoFinal.m

--- ALGEBRA/PROYECTOFINAL/ProyectoFinal.m
+++ ALGEBRA/PROYECTOFINAL/ProyectoFinal.m
@@ -16,6 +16,7 @@ T = [1/3,0,0,1/3,0,0,0,0,0;
 #MATRIZ INVERSA DE T:
 invT = inv(T)
 
+#Ejercicio 1:
 [L,U,P]=lu(invT)
 b=[0;1;0;0;0;0;0;0;0]; #Vector b = x0
 b1=P*b;
@@ -34,4 +35,17 @@ for i=10   #Son 10 etapas, por tanto nos interesa la décima iteración
   disp(x); #Mostramos el resultado de la variable "y10" por pantalla
 endfor
 
+#Multiplicar resultado zona 2 de x e y
+#Multiplicar resultado zona 5 de x e y
 
+#Ejercicio 2:
+#Ponemos la columna 5 como el vector z0
+#Zona 3 como x0 por tanto
+b=[0;0;1;0;0;0;0;0;0]
+#Finalmente multiplicamos el resultado de la zona 1 de x e y
+
+#Ejercicio 3:
+#Para responder a este apartado, tenemos que encontrar el mayor de los valores multiplicados entre x e y
+
+#Ejercicio 4:
+#Multiplicamos x y z para cada una de las zonas
