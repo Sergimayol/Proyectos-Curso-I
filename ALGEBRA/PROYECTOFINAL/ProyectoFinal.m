ba4a52ef2622fc863b59f9328df7380a5e911d1d
alerodriargui <80339818+alerodriargui@users.noreply.github.com>
2021-05-23 21:01:53 +0200

    Update ProyectoFinal.m

--- ALGEBRA/PROYECTOFINAL/ProyectoFinal.m
+++ ALGEBRA/PROYECTOFINAL/ProyectoFinal.m
@@ -16,3 +16,23 @@ T = [1/3,0,0,1/3,0,0,0,0,0;
 #MATRIZ INVERSA DE T:
 invT = inv(T)
 
+[L,U,P]=lu((inv(T));
+b=[0;1;0;0;0;0;0;0;0]; #Vector b = x0
+b1=P*b;
+y=susprog(L,b1);
+x=susreg(U,y);
+for i=10   #Son 10 etapas, por tanto nos interesa la décima iteración
+  disp(x); #Mostramos el resultado de la variable "x10" por pantalla
+endfor
+
+[L,U,P]=lu((inv(T));
+b=[0;0;0;0;0;0;0;1;0]; #Vector b = y0
+b1=P*b;
+y=susprog(L,b1);
+x=susreg(U,y);
+for i=10   #Son 10 etapas, por tanto nos interesa la décima iteración
+  disp(x); #Mostramos el resultado de la variable "y10" por pantalla
+endfor
+
+
+
